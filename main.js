30d33090050b72944ac5bf6c797e93fd8b53c99a
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-10 11:13:54 +0300

    Lisatud event-i kuulaja(listener).

---
+++ main.js
@@ -0,0 +1,8 @@
+//DOM Elements
+let kgInput = document.getElementById('kgInput');
+
+//Listening to events
+kgInput.addEventListener('input', event => {
+    let kg = event.target.value;
+    console.log(kg);
+});
