28d050708eb73a019ee1b2eca03795589aeba122
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-10 11:40:27 +0300

    Parandatud gOutput.innerHTML

--- main.js
+++ main.js
@@ -5,5 +5,5 @@ let gramOutput = document.querySelector('#gOutput');
 //Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
-    gramOutput.innerHTML = `$(kg * 1000 g`);
+    gOutput.innerHTML = `${kg * 1000} g`;
 });
