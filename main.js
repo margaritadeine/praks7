6dcbeb4f4ec9e937688b98c5bbd693b3dcf64305
Margarita Deinekina <margarita.deinekina@khk.ee>
2018-10-10 12:33:46 +0300

    Lisatud naelade ja untside kalkuleerimise võimalus

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 //DOM Elements
-let kgInput = document.getElementById('kgInput');
-let gramOutput = document.querySelector('#gOutput');
-
+let kgInput = document.querySelector('#kgInput');
+let gOutput = document.querySelector('#gOutput');
+let pOutput = document.getElementById('pOutput');
+let zOutput = document.getElementById('zOutput');
 //Listening to events
 kgInput.addEventListener('input', event => {
     let kg = event.target.value;
     gOutput.innerHTML = `${kg * 1000} g`;
+    pOutput.innerHTML = `${kg * 2.20462262} lb`;
+    zOutput.innerHTML = `${kg * 35.2739619} oz`;
 });
